1ae4b85d3f97b5514ec10625fc92659d27f4fba4
Gogs <gogs@fake.local>
2022-10-03 21:45:48 -0300

    me voy a cenar

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="blod_index"),
+    path('', views.index, name="blog_index"),
+    path('about', views.about, name="blog_about"),
+    path('contacto', views.contacto, name="blog_contacto"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog_index.html')
+
+def about(request):
+    return render(request, 'blog_about.html')
+
+def contacto(request):
+    return render(request, 'blog_contacto.html')
