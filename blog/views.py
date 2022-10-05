231f26794a1163432418759a6d2cc9ab8f26983a
Lycha156 <andradamauricio87@gmail.com>
2022-10-05 12:46:41 -0300

    me fui

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 def index(request):
-    posts = Post.objects.all()
+    if request.method == 'POST':
+        query = request.POST.get('q')
+        postsObjects = Post.objects.filter( Q(titulo__contains = query) | Q(fechaPublicacion__contains = query))[:25]
+    else:
+        posts = Post.objects.all()
+
+        pagination = Paginator(posts, 2)
+        page_number = request.GET.get('page')
+        postsObjects = pagination.get_page(page_number)
 
     context = {
-        'posts': posts
+        'posts': postsObjects,
     }
         
     return render(request, 'blog_index.html', context)
